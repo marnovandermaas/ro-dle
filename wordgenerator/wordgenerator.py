7f8f8b78aac8a3e7c828f731f211abe15e0aabb0
Marno <git@techroose.com>
2022-02-12 14:22:37 +0000

    Generating wordlist.ts

--- wordgenerator/wordgenerator.py
+++ wordgenerator/wordgenerator.py
@@ -1,9 +1,11 @@
 wordfile = open('nohyphen.ro.txt')
 
+# Populate words
 words = []
 for line in wordfile.readlines():
     words.append(line[0:5].lower())
 
+# Replace ă with a
 total = 0
 alreadyin = 0
 guesses = set()
@@ -19,17 +21,26 @@ for word in words:
 
 print('Total words with ă is ' + str(total) + ' of which ' + str(alreadyin) + ' exist already in the list when substituting with a.')
 
+# Output validGuesses.ts
 guessfile = open('validGuesses.ts', 'w')
 guessfile.write('export const VALID_GUESSES = [\n')
 for word in sorted(guesses):
     guessfile.write("  '" + word + "',\n")
 guessfile.write(']\n')
 
+# Process most common Romanian words
 import csv
 commonfile = open('2000MostCommonRomanianWords.csv')
 csvFile = csv.reader(commonfile)
+answers = set()
 for line in csvFile:
     word = line[1]
     if len(word) == 5:
-        print(word)
+        answers.add(word.lower().replace('ă', 'a')
 
+# Output wordlist.ts
+answerfile = open('wordlist.ts', 'w')
+wordfile.write('export const WORDS = [\n')
+for word in answers:
+    answerfile.write("  '" + word + "',\n")
+answerfile.write(']\n')
