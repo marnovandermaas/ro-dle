c65bff87321b91b1c879c5914ec1276aa1953b64
Marno <git@techroose.com>
2022-02-12 14:31:07 +0000

    Add extra Romanian letters to keyboard

--- src/components/keyboard/Keyboard.tsx
+++ src/components/keyboard/Keyboard.tsx
@@ -52,7 +52,7 @@ export const Keyboard = ({
   return (
     <div>
       <div className="flex justify-center mb-1">
-        {['Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P'].map((key) => (
+        {['Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', 'Ș', 'Ț'].map((key) => (
           <Key
             value={key}
             key={key}
@@ -63,7 +63,7 @@ export const Keyboard = ({
         ))}
       </div>
       <div className="flex justify-center mb-1">
-        {['A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L'].map((key) => (
+        {['A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'Â', 'Î'].map((key) => (
           <Key
             value={key}
             key={key}
