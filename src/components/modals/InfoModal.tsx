ccdc0098bb821a279f78d8cdc2423fefcab7737b
Marno van der Maas <34654485+marnovandermaas@users.noreply.github.com>
2022-02-12 10:57:25 +0000

    Translation of InfoModal provided by @idv20

--- src/components/modals/InfoModal.tsx
+++ src/components/modals/InfoModal.tsx
@@ -8,21 +8,21 @@ type Props = {
 
 export const InfoModal = ({ isOpen, handleClose }: Props) => {
   return (
-    <BaseModal title="How to play" isOpen={isOpen} handleClose={handleClose}>
+    <BaseModal title="Regulile jocului" isOpen={isOpen} handleClose={handleClose}>
       <p className="text-sm text-gray-500 dark:text-gray-300">
-        Guess the word in 6 tries. After each guess, the color of the tiles will
-        change to show how close your guess was to the word.
+        Ghiceste cuvantul din 6 incercari. Dupa fiecare incercare, culoarea patratelor se va schimba depinzand de cat
+        de aproape ai fost de a ghici cuvantul.
       </p>
 
       <div className="flex justify-center mb-1 mt-4">
-        <Cell value="W" status="correct" />
-        <Cell value="E" />
+        <Cell value="S" status="correct" />
+        <Cell value="C" />
         <Cell value="A" />
-        <Cell value="R" />
-        <Cell value="Y" />
+        <Cell value="U" />
+        <Cell value="N" />
       </div>
       <p className="text-sm text-gray-500 dark:text-gray-300">
-        The letter W is in the word and in the correct spot.
+        Litera S se afla in cuvant si este in pozitia corecta.
       </p>
 
       <div className="flex justify-center mb-1 mt-4">
@@ -33,18 +33,18 @@ export const InfoModal = ({ isOpen, handleClose }: Props) => {
         <Cell value="T" />
       </div>
       <p className="text-sm text-gray-500 dark:text-gray-300">
-        The letter L is in the word but in the wrong spot.
+        Litera L se afla in cuvant dar este in pozitia gresita.
       </p>
 
       <div className="flex justify-center mb-1 mt-4">
-        <Cell value="V" />
-        <Cell value="A" />
-        <Cell value="G" />
-        <Cell value="U" status="absent" />
+        <Cell value="M" />
+        <Cell value="I" />
+        <Cell value="N" />
+        <Cell value="G" status="absent" />
         <Cell value="E" />
       </div>
       <p className="text-sm text-gray-500 dark:text-gray-300">
-        The letter U is not in the word in any spot.
+        Litera G nu se afla in cuvant.
       </p>
     </BaseModal>
   )
